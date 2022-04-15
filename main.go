1c85a0868d55f2735e501688d98e4829451ec601
wsjcko <wsjcko@163.com>
2022-04-15 21:16:01 +0800

    micro_addr

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ import (
 var (
 	MICRO_SERVICE_NAME    = "go.micro.service.shop.payment"
 	MICRO_VERSION         = "latest"
-	MICRO_ADDRESS         = "0.0.0.0:8089"
+	MICRO_ADDRESS         = "0.0.0.0:8090"
 	MICRO_QPS             = 100
 	DOCKER_HOST           = "127.0.0.1"
 	MICRO_CONSUL_ADDRESS  = "127.0.0.1:8500"
